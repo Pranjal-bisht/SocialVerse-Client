9b78c8c577ab38a21b0051a435672f4bce8ecbb8
Pranjal Bisht <bishtpranjal27@gmail.com>
2021-11-09 00:46:55 +0530

    Added frontend of the web app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import NavBar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/screens/Home'
+import Signin from './components/screens/Login';
+import Profile from './components/screens/Profile';
+import Signup from './components/screens/Signup';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+const NavBar = () => {
+    return (
+        <>
+            <nav>
+                <div className="nav-wrapper white">
+                    <Link to="/" className="brand-logo left"><h4>SocialVerse</h4></Link>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li><Link to="/signin">signin</Link></li>
+                        <li><Link to="/signup">Signup</Link></li>
+                        <li><Link to="/profile">Profile</Link></li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar
