07cfea46530fc0fcebdffd62fceb4c2048bab0fb
Pranjal Bisht <bishtpranjal27@gmail.com>
2021-11-09 18:08:11 +0530

    Connected frontend with backend API's and fixed all isuues

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import React from 'react';
+import React,{useEffect,createContext,useReducer,useContext} from 'react';
 import './App.css';
 import NavBar from './components/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter,Route,Routes, useNavigate} from 'react-router-dom'
 import Home from './components/screens/Home'
-import Signin from './components/screens/Login';
+import Signin from './components/screens/SignIn';
 import Profile from './components/screens/Profile';
 import Signup from './components/screens/Signup';
-function App() {
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
+import {reducer,initialState} from './reducers/userReducer'
+import CreatePost from './components/screens/CreatePost'
+export const UserContext = createContext()
+const Routing = ()=>{
+  const history = useNavigate()
+  const {state, dispatch} = useContext(UserContext)
+  useEffect(()=>{
+    const user = JSON.parse(localStorage.getItem("user"))
+    if(user){
+      dispatch({type:"USER",payload:user})
+    }else{
+      // if(!history.location.pathname.startsWith('/reset'))
+      //      history.push('/signin')
+      if(user){
+        history('/')
+      }
+      else
+      {
+        history('/signin')
+      }
+    }
+  },[])
+  return(
+    <Routes>
+      <Route exact path="/" element={<Home />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
-      </Routes>
+        <Route path="/create" element={<CreatePost />} />
+    </Routes>
+  )
+}
+
+function App() {
+  const [state,dispatch] = useReducer(reducer,initialState)
+  return (
+    <UserContext.Provider value={{state,dispatch}}>
+    <BrowserRouter>
+      <NavBar />
+      <Routing />
+      
     </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
